3d815a22cd82044cce3607e5ab16102f8897324c
usamaimdad <rreigns273@gmail.com>
2021-01-09 13:51:05 +0500

    Fixed Description Error

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="subnet",
-    version="0.0.1",
+    version="0.0.2",
     author="Usama Imdad",
     author_email="usamaimdadsian@gmail.com",
-    description="A small example package",
+    description="A package to subnet ip address for static or dynamic networking.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/usamaimdadsian/subnet-pkg",
